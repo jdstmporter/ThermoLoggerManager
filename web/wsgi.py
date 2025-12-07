d159a6ac4aca19208fd225b2b3e902ed30f376b9
Julian Porter <julian@porternet.org.uk>
2025-12-07 17:53:26 +0000

    get rid of apache specific

--- web/wsgi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from web.common import Params
-from web.wsgi import WSGIApp
-
-# need to set pythonpath in here, so it includes /usr/local/src/web
-
-params = Params.load('/etc/thermologger/config.json')
-application = WSGIApp(params)
